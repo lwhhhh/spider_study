ed00e6398c609f8da1a9b81a013dd8d2367d7fa6
lwh <lwhile@outlook.com>
2016-05-04 23:54:59 +0800

    back up

--- pm2.5_zhaoqing.py
+++ pm2.5_zhaoqing.py
@@ -1,13 +1,25 @@
+import os
 import urllib.request
+import json
 
 '''
     example: http://www.pm25.in/api/querys/pm2_5.json?city=珠海&token=xxxxxx
 '''
 
 BASE_URL = 'http://www.pm25.in/api/querys/aqi_ranking.json'
+SAVE_PATH = os.getcwd() + '/response_data'
 
+def save_json(response_data):
+    '''
+        API只能调用一次
+        珍惜机会所以把下载到的数据先保存一份到磁盘上
+    '''
+    with open(SAVE_PATH,w) as fp:
+        fp.write(response_data)
+    
 def dowload(url):
     response_data = urllib.request.urlopen(url).read()
+    save_json(response_data)
     response_data = response_data.decode('utf8')
     return response_data
     
